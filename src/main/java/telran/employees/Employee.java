b0e98c85e6994d8fdfff635c5e98437bbd4a83a5
yanfi-sher <loss.soul20@gmail.com>
2024-11-09 12:02:41 +0200

    change employee

--- src/main/java/telran/employees/Employee.java
+++ src/main/java/telran/employees/Employee.java
@@ -3,59 +3,56 @@ package telran.employees;
 import org.json.JSONObject;
 
 public class Employee {
-
-    private Long id;
-    private int salary;
+    private long id;
+    private int basicSalary;
     private String department;
+    public Employee(){
 
-    public Employee() {
     }
-    
     @SuppressWarnings("unchecked")
-    static public Employee getEmployeeFromJSON(String jsonSTR) {
-        JSONObject jsonObj = new JSONObject(jsonSTR);
+    static public Employee getEmployeeFromJSON(String jsonStr) {
+        JSONObject jsonObj = new JSONObject(jsonStr);
         String className = jsonObj.getString("className");
         try {
             Class<Employee> clazz = (Class<Employee>) Class.forName(className);
-            Employee empl = clazz.getConstructor().newInstance();
+            Employee empl =  clazz.getConstructor().newInstance();
             empl.setObject(jsonObj);
             return empl;
         } catch (Exception e) {
-            throw new RuntimeException(e);        
-        }   
+           throw new RuntimeException(e);
+        }
     }
-
-    protected void setObject(JSONObject jsonObj) {
-        id = jsonObj.getLong("id");
-        salary = jsonObj.getInt("basicSalary");
-        department = jsonObj.getString("department");
+    
+    public int getBasicSalary() {
+        return basicSalary;
     }
-
-    public Employee(Long id, int salary, String department) {
+   protected void setObject(JSONObject jsonObj) {
+       id = jsonObj.getLong("id");
+       basicSalary = jsonObj.getInt("basicSalary");
+       department = jsonObj.getString("department");
+    }
+    public Employee(long id, int basicSalary, String department) {
         this.id = id;
-        this.salary = salary;
+        this.basicSalary = basicSalary;
         this.department = department;
     }
-
-    public Long getId() {
+    public int computeSalary() {
+        return basicSalary;
+    }
+    public long getId() {
         return id;
     }
-
-    public int computeSalary() {
-        return salary;
-    }
-
     public String getDepartment() {
         return department;
     }
-
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        Employee employee = (Employee) obj;
-        return employee==null? false : id == employee.id && salary == employee.salary;
+       boolean res = false;
+       if (obj instanceof Employee empl) {
+            res = id == empl.id;
+       }
+       return res;
     }
-
     @Override
     public String toString() {
         JSONObject jsonObj = new JSONObject();
@@ -63,15 +60,9 @@ public class Employee {
         fillJSON(jsonObj);
         return jsonObj.toString();
     }
-
-    protected void fillJSON(JSONObject jsonObj) {
-        jsonObj.put("id", id);
-        jsonObj.put("basicSalary", salary);
+   protected void fillJSON(JSONObject jsonObj) {
+        jsonObj.put("id",id);
+        jsonObj.put("basicSalary", basicSalary);
         jsonObj.put("department", department);
     }
-
-
-
-    
-
 }
--- src/main/java/telran/employees/WageEmployee.java
+++ src/main/java/telran/employees/WageEmployee.java
@@ -3,39 +3,34 @@ package telran.employees;
 import org.json.JSONObject;
 
 public class WageEmployee extends Employee{
-
-    int wage;
-    int hours;
-
-    public WageEmployee() {
-    }
-
-    @Override
-    protected void setObject(JSONObject jsonObj) {
-        super.setObject(jsonObj);
-        wage = jsonObj.getInt("wage");
-        hours = jsonObj.getInt("hours");
-
-    }
-
-    public WageEmployee(Long id, int salary, String department, int wage, int hours) {
-        super(id, salary, department);
+    private int wage;
+    private int hours;
+    public WageEmployee(){}
+    public WageEmployee(long id, int basicSalary, String department, int wage, int hours) {
+        super(id, basicSalary, department);
         this.wage = wage;
         this.hours = hours;
     }
-
     @Override
-    public int computeSalary(){
-        return super.computeSalary()+wage*hours;
+    public int computeSalary() {
+        return super.computeSalary() + wage * hours;
     }
-
-        @Override
-    protected void fillJSON(JSONObject jsonObj) {
-        super.fillJSON(jsonObj);
-        jsonObj.put("wage", wage);
-        jsonObj.put("hours", hours);
-    }    
-
-
-
+    @Override
+     protected void fillJSON(JSONObject jsonObj) {
+      super.fillJSON(jsonObj);
+      jsonObj.put("wage", wage);
+      jsonObj.put("hours", hours);
+     }
+     @Override
+     protected void setObject(JSONObject jsonObj) {
+         super.setObject(jsonObj);
+         wage = jsonObj.getInt("wage");
+         hours = jsonObj.getInt("hours");
+      }
+      public int getHours() {
+          return hours;
+      }
+      public int getWage() {
+          return wage;
+      }
 }
